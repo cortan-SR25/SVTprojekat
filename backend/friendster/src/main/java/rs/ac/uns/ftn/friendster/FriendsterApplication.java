ac4e12ad57a60e474464491dbd9745f57ee79727
cortan-SR25 <80309754+cortan-SR25@users.noreply.github.com>
2023-05-07 18:05:21 +0200

    Created project backend.

---
+++ backend/friendster/src/main/java/rs/ac/uns/ftn/friendster/FriendsterApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.friendster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendsterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendsterApplication.class, args);
+	}
+
+}
